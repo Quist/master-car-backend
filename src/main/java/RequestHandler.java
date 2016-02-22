16450968aa469ca42486eaacaa0d8f5a52e17918
Joakim Lindquister <joakiml@gmail.com>
2016-02-22 22:08:38 +0100

    Log headers

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -2,6 +2,8 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
 import java.io.OutputStream;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
 
 class RequestHandler implements HttpHandler{
 	
@@ -9,7 +11,16 @@ class RequestHandler implements HttpHandler{
 
 		System.out.println("Received request: " + exchange.getRequestURI());
 		System.out.println("Remote address: " + exchange.getRemoteAddress());
+		System.out.println("Method: " + exchange.getRequestMethod());
 
+		Set<String> headers = exchange.getRequestHeaders().keySet();
+		System.out.println("Headers:");
+
+		Iterator<String> iterator = headers.iterator();
+		while(iterator.hasNext()) {
+            String key = iterator.next();
+			System.out.println(key + ": " + exchange.getRequestHeaders().get(key));
+		}
 
 		String response = "Hei du ;))";
 		exchange.sendResponseHeaders(200, response.length());
